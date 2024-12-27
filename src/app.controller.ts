a66680d2623675465a90240c08d0f04de02d4f1a
dongzhiyang <dongzhiyang@honghe-tech.com>
2024-12-27 14:57:48 +0800

    feat:jwt实现认证

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,49 @@
-import { Controller, Get, Session } from '@nestjs/common';
+import { Controller, Get, Headers, Inject, Res, Session, UnauthorizedException } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  // @Get('ttt')
+  // ttt(@Res({ passthrough: true }) response: Response) {
+  //     const newToken = this.jwtService.sign({
+  //       count: 1
+  //     });
+
+  //     response.setHeader('token', newToken);
+  //     return 'hello';
+  // }
+  @Get('ttt')
+  ttt(@Headers('authorization') authorization: string, @Res({ passthrough: true}) response: Response) {
+      if(authorization) {
+        try {
+          const token = authorization.split(' ')[1];
+          const data = this.jwtService.verify(token);
+
+          const newToken = this.jwtService.sign({
+            count: data.count + 1
+          });
+          response.setHeader('token', newToken);
+          return data.count + 1
+        } catch(e) {
+          console.log(e);
+          throw new UnauthorizedException();
+        }
+      } else {
+        const newToken = this.jwtService.sign({
+          count: 1
+        });
+
+        response.setHeader('token', newToken);
+        return 1;
+      }
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    // JwtModule.register({
+    //   secret: 'dong',
+    //   signOptions: {
+    //     expiresIn: '7d'
+    //   }
+    // })
+    JwtModule.registerAsync({
+      async useFactory() {
+        await 111;
+        return {
+          secret:'dong',
+          signOptions: {
+            expiresIn: '7d'
+          }
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
